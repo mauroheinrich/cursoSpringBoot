61c44f2331a965939edeb702fc69665636389842
Mauro Jesus Heinrich <heinrichmauro@gmail.com>
2024-05-21 11:46:59 -0300

    metodo post

---
+++ cursoSpringBoot/src/main/java/ar/com/mauroheinrich/cursoSpringBoot/Cliente.java
@@ -0,0 +1,12 @@
+
+package ar.com.mauroheinrich.cursoSpringBoot;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Cliente {
+    private Long id;
+    private String nombre;
+   private String apellido;
+}
